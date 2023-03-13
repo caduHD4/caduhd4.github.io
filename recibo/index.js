91c4864a598d8bd8d214cce7aaea1e15a13fdcef
caduHD4 <cadumarianogarciapereira@gmail.com>
2023-03-13 20:24:05 -0300

    ta ruim

--- recibo/index.js
+++ recibo/index.js
@@ -2,6 +2,12 @@ const camposCartao = document.getElementById("camposCartao");
 const camposDinheiro = document.getElementById("camposDinheiro");
 const cartao = document.getElementById("cartao");
 const dinheiro = document.getElementById("dinheiro");
+const pix = document.getElementById("pix");
+const camposPix = document.getElementById("camposPix");
+const cheque = document.getElementById("cheque");
+const camposCheque = document.getElementById("camposCheque");
+const trans = document.getElementById("trans");
+const camposTrans = document.getElementById("camposTrans");
 
 cartao.addEventListener("change", function() {
   if (cartao.checked) {
@@ -15,11 +21,33 @@ cartao.addEventListener("change", function() {
 dinheiro.addEventListener("change", function() {
   if (dinheiro.checked) {
     camposCartao.style.display = "none";
-    // exibe a div do dinheiro
     camposDinheiro.style.display = "block";
   } else {
     camposDinheiro.style.display = "none";
   }
 });
 
+pix.addEventListener("change", function () {
+  if (pix.checked) {
+    camposDinheiro.style.display = "none";
+    camposPix.style.display = "block";
+  } else {
+    camposPix.style.display = "none";
+  }
+});
 
+cheque.addEventListener("change", function () {
+  if (cheque.checked) {
+    camposCheque.style.display = "block";
+  } else {
+    camposCheque.style.display = "none";
+  }
+});
+
+trans.addEventListener("change", function () {
+  if (trans.checked) {
+    camposTrans.style.display = "block";
+  } else {
+    camposTrans.style.display = "none";
+  }
+});
